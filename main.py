85bad4aa14475e460f0bc2f79f76a65312fc1785
kottycomputer <kottycomputer@outlook.jp>
2022-10-19 14:44:30 +0900

    assert"What is your name?"

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == '__main__':
+    name = input("What is your name?")
     print_hi('koto')
 
 # PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
